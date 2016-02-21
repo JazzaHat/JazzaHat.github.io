ecdc5de25bfdda83bb9eb41be6c7a0a0752126b4
JazzaHat <jazzah98@gmail.com>
2016-02-21 13:43:40 +0800

    Update website to look better
    -Updated style sheets
    -Fixed alignment issues
    -Updated Logo
    -Updated Slider code to work properly
    -Removed Facebook icon
    -Added x328 ISA Image for Slider

--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,7 @@
 /*Slider*/
-var sliders = 5;
+var sliders = 3;
 var currentSlide = 1;
+document.getElementById("slide_ind_" + currentSlide.toString()).classList.add('active');
 /*Show and hide slider controls*/
 function showSliderControls(){
 	document.getElementById("next_slide").style.marginLeft="-42px";
@@ -17,14 +18,16 @@ function hideSliderControls(){
 function gotoImage (n) {
 	if (n < 1) {n = sliders};
 	if (n > sliders) {n = 1};
-	console.log(n)
+	//console.log(n)
 	var a = "slide_ind_" + currentSlide.toString();
 	var b = "slide_ind_" + n.toString();
-	document.getElementById(a).style.backgroundColor="#0087BD";/*"Uncheck" current slide*/
-	document.getElementById(b).style.backgroundColor="#DF0024";/*"Check" new slide*/
+	document.getElementById(a).classList.remove('active');
+	document.getElementById(b).classList.add('active');
+	/*document.getElementById(a).style.backgroundColor="";/*"Uncheck" current slide*/
+	/*document.getElementById(b).style.backgroundColor="#DF0024";/*"Check" new slide*/
 	currentSlide = n
 	var c = -100*(currentSlide-1);
 	c = c + "%";
-	console.log(c)
+	//console.log(c)
 	document.getElementById("slide_img_holder").style.marginLeft=c;
 }
